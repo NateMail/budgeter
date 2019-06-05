d85db12552ed4049fa50558852a52a840a05f47a
nathanmailhiot <nathanjm12@outlook.com>
2019-06-05 11:48:55 -0400

    Now can delete incomes

--- src/components/routes/Incomes.js
+++ src/components/routes/Incomes.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 // import { Link } from 'react-router-dom'
 import axios from 'axios'
+import { withRouter, Redirect } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
 
 import apiUrl from '../../apiConfig'
 
@@ -9,7 +11,8 @@ class Incomes extends Component {
     super(props)
 
     this.state = {
-      monthly: []
+      monthly: [],
+      deleted: false
     }
   }
 
@@ -28,13 +31,51 @@ class Incomes extends Component {
       .catch(console.error)
   }
 
+  handleDelete = (id) => {
+    axios({
+      url: `${apiUrl}/monthlies/${id}`,
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token token=${this.props.user.token}`
+      }
+    })
+      .then(() => {
+        this.setState({ deleted: true })
+      })
+      .catch(console.error)
+  }
+
   render () {
     const monthly = this.state.monthly.map(monthly => (
       <li key={monthly.id}>
         <h3>  {monthly.income} </h3>
         <h5>  {monthly.deposited} </h5>
+        <Button
+          variant="danger"
+          type="button"
+          className="m-1"
+          onClick={() => this.handleDelete(monthly.id)}
+        >
+        Delete
+        </Button>
       </li>
     ))
+
+    const { deleted } = this.state
+
+    if (!monthly) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )
+    }
+
+    if (deleted) {
+      return <Redirect to={
+        { pathname: '/', state: { msg: 'Income Successfully deleted!' } }
+      } />
+    }
     return (
       <div>
         <h3>All the Incomes</h3>
@@ -44,4 +85,4 @@ class Incomes extends Component {
   }
 }
 
-export default Incomes
+export default withRouter(Incomes)
